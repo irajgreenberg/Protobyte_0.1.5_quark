203ccdfb0655a8396153c2cca8da4d7f54a22519
irajgreenberg <irajgreenberg@gmail.com>
2015-06-22 07:32:33 -0500

    completed bash ProjectCreator script

--- Protobyte/ProtoCreator/ProtoCreator.bat
+++
@@ -1,4 +0,0 @@
-XCOPY /a /b /e /f /g /s ..\ProtoTemplate\* ..\Projects 
-rename ..\Projects\ProtoApp\ProtoApp.* %1.*
-rename ..\Projects\ProtoApp %1
-
--- Protobyte/ProtoCreator/ProtoCreator.sh
+++ Protobyte/ProtoCreator/ProtoCreator.sh
@@ -5,15 +5,25 @@
 
 cp  -r ../ProtoTemplate/__Protobyte_Project_Template__ ../Projects
 cd ../Projects
-mv __Protobyte_Project_Template__ $1
-cd $1
-mv *.vcxproj $1.vcxproj
-mv *.vcxproj.filters $1.vcxproj.filters
+
+projName=$1
+
+if [ -z "$projName" ]
+  then
+  	#http://stackoverflow.com/questions/1401482/yyyy-mm-dd-format-date-in-shell-script
+  	DATE=`date +%Y%m%d%H%M%S`
+    projName="sketch_"$DATE
+fi
+
+mv __Protobyte_Project_Template__ $projName
+cd $projName
+mv *.vcxproj $projName.vcxproj
+mv *.vcxproj.filters $projName.vcxproj.filters
 echo "\"Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 2013
 VisualStudioVersion = 12.0.21005.1
 MinimumVisualStudioVersion = 10.0.40219.1
-Project("\"{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}\"") = "\"$1\"", "\"$1.vcxproj\"", "\"{4DF3E3D9-2197-48F1-80E4-3F0E3E408694}\""
+Project("\"{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}\"") = "\"$projName\"", "\"$projName.vcxproj\"", "\"{4DF3E3D9-2197-48F1-80E4-3F0E3E408694}\""
 EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
@@ -29,5 +39,5 @@ Global
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
-EndGlobal\"" > $1.sln
+EndGlobal\"" > $projName.sln
 ls -a
