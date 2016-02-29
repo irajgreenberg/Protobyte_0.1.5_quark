761baa012a67a681d7ed38d9aec2bd2058e5eb40
Brittni Watkins <blwatkins@smu.edu>
2016-02-29 16:15:16 -0600

    Clean up branch. Delete old projects. Add protobyte gui files. Add resources folder to protocreator gui tool.

--- Protobyte/Projects/Constructor_Development/ProtoMain.cpp
+++
@@ -1,7 +0,0 @@
-#include "ProtoPlasm.h"
-#include "ProtoController.h"
-
-int main(int argc, char const** argv) {
-		ijg::ProtoPlasm p(600, 600, "ProtoController", new ProtoController());
-		return EXIT_SUCCESS;
-}
--- Protobyte/Projects/ObjectTemplate01/ProtoMain.cpp
+++
@@ -1,7 +0,0 @@
-#include "ProtoPlasm.h"
-#include "ProtoController.h"
-
-int main(int argc, char const** argv) {
-		ijg::ProtoPlasm p(1300, 600, "ProtoController", new ProtoController());
-		return EXIT_SUCCESS;
-}
